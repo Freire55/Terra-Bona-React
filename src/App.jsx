2ca1b73fcaf882cf87555c0b1442e68d391ca71c
Tomás <noronha.t@sapo.pt>
2023-01-15 01:45:25 +0000

    Intro

---
+++ src/App.jsx
@@ -0,0 +1,127 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+            <div class="nav-bar">
+          <img
+          class="logo"
+          src="terra_bona_logo-removebg-preview.png"
+          alt="Terra Bona Logo"
+          />
+          <div class="langs">
+              <a class="lang" href="indexpt.html"> Português </a>
+              <a class="lang" href="indexpt.html"> English </a>
+              <a class="lang" href="indexpt.html"> Français </a>
+            </div>
+            <div class="sign-up">
+                <form action="./loginpt.html" target="_blank">
+                    <button class="login">
+                        Login
+                    </button>
+                </form>
+                <form action="./signuppt.html" target="_blank">
+                    <button class="login">
+                        Registar
+                    </button>
+                </form>
+            </div>
+        </div>
+            <div class="view-img">
+                <div class="parag">
+                    <p class="read-more">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit necessitatibus quaerat quasi temporibus id, eius aliquam, explicabo, ullam laudantium dolorem nam eligendi minima reprehenderit blanditiis molestias. Ut molestiae exercitationem maiores!</p>
+                    <p class="read-more">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique veritatis molestiae nemo incidunt odio dolores placeat possimus fugiat, corporis officiis.</p>
+                    <p class="read-more">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, assumenda.</p>
+                </div>
+            </div>
+        <main class="main">
+            <div class="topics">
+                <a href="#aboutus">
+                    <button class="links">Sobre Nós</button>
+                </a>
+                <a href="#vinha">
+                    <button class="links">A Vinha</button>
+                </a>
+                <a href="#villas">
+                    <button class="links">Villas</button>
+                </a>
+                <a href="#experiences">
+                    <button class="links">Experiências</button>
+                </a>
+                <a href="#boaventura">
+                    <button class="links">A Boaventura</button>
+                </a>
+                <a href="#noticias">
+                    <button class="links">Notícias</button>
+                </a>
+            </div>
+            <section class="themes" id="aboutus">
+                <h1>Sobre Nós</h1>
+                <div class="content">
+                    <div class="content-p">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                    </div>
+                </div>
+            </section>
+            <section class="themes" id="vinha">
+                <h1>A Vinha</h1>
+                <div class="content">
+                    <div class="content-p">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                    </div>
+                </div>
+            </section>
+            <section class="themes" id="villas">
+                <h1>Villas</h1>
+                <div class="content">
+                    <div class="content-p">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                    </div>
+                </div>
+            </section>
+            <section class="themes" id="experiences">
+                <h1>Experiências</h1>
+                <div class="content">
+                    <div class="content-p">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                    </div>
+                </div>
+            </section>
+            <section class="themes" id="boaventura">
+                <h1>Boaventura</h1>
+                <div class="content">
+                    <div class="content-p">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                    </div>
+                </div>
+            </section>
+            <section class="themes" id="noticias">
+                <h1>Notícias</h1>
+                <div class="content">
+                    <div class="content-p">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel voluptatem modi harum ratione voluptates, a ipsam impedit omnis. Cumque officiis molestiae voluptate suscipit quo explicabo quis tempore sed totam? Blanditiis ipsa cupiditate ut. Molestias illo unde qui aspernatur iste autem? Alias, rem atque recusandae delectus distinctio veritatis ipsum perspiciatis, reprehenderit nihil dolore aperiam. Commodi minus omnis illum laborum voluptas dolores, provident ipsum debitis vel dolorem aliquam explicabo blanditiis aliquid?</p>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
+  );
+}
+
+export default App;
